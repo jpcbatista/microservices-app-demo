9b7ed731acd2f6b4bddc40ed78aabfb644fa0351
joao pedro <jpcbatista2@gmail.com>
2021-03-11 18:18:58 -0300

    Project hr-oauth created

---
+++ hr-oauth/src/test/java/com/projetos/jp/hroauth/HrOauthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projetos.jp.hroauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrOauthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
