69d1bba80ba8cccb88ab903618a4729b827fb639
joao pedro <jpcbatista2@gmail.com>
2021-03-11 15:46:49 -0300

    Project hr-user created

---
+++ hr-user/src/main/java/com/projetos/jp/hrhruser/HrUserApplication.java
@@ -0,0 +1,13 @@
+package com.projetos.jp.hrhruser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrUserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HrUserApplication.class, args);
+	}
+
+}
---
+++ hr-user/src/test/java/com/projetos/jp/hrhruser/HrUserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projetos.jp.hrhruser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrUserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
